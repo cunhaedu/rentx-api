f47cee7540e4729d49b5b46d935a6bc2f531b83b
cunhaedu <cunhaeduardo1231@gmail.com>
2021-12-12 19:16:01 -0300

    feat(car): add migration to create car table

---
+++ src/shared/infra/typeorm/migrations/1639346251877-create_table_car.ts
@@ -0,0 +1,85 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class createTableCar1639346251877 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'car',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'daily_rate',
+            type: 'numeric',
+          },
+          {
+            name: 'available',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'license_plate',
+            type: 'varchar',
+          },
+          {
+            name: 'fine_amount',
+            type: 'numeric',
+          },
+          {
+            name: 'brand',
+            type: 'numeric',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            onUpdate: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            default: null,
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'car_category_fk',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'category',
+            onUpdate: 'SET NULL',
+            onDelete: 'SET NULL',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('car');
+  }
+}
