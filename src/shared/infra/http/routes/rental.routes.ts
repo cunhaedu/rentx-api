cc6ae866151e3e475f06cde73bdbbdb0b3c3b41b
cunhaedu <cunhaeduardo1231@gmail.com>
2022-01-23 13:10:33 -0300

    feat(rental): implements devolution use case

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -2,11 +2,18 @@ import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rental/useCases/CreateRental/CreateRentalController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares';
+import { DevolutionRentalController } from '@modules/rental/useCases/DevolutionRental/DevolutionRentalController';
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 const rentalRoutes = Router();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  '/devolution/:rentalId',
+  ensureAuthenticated,
+  devolutionRentalController.handle,
+);
 
 export { rentalRoutes };
